7eec6f800f774638c0e3c0e6656e20b7e73e07c2
Hakmi Rais Fauzan <69943045+raisfauzan@users.noreply.github.com>
2025-11-10 08:29:09 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 /*==================== typed js ====================*/
 const typed = new Typed(".multiple-text", {
-  strings: ["FrontEnd Developer", "Researcher", "Calon Dosen"],
+  strings: ["FrontEnd Developer", "Peneliti", "Dosen Pendidikan Matematika"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
